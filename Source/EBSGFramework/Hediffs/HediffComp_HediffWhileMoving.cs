6b68277acf019cd7ee07779734d44c3ea6e0e80d
Robert <robhos2000@gmail.com>
2023-12-21 11:37:56 -0500

    Fade with moving hediffs updated

--- Source/EBSGFramework/Hediffs/HediffComp_HediffWhileMoving.cs
+++ Source/EBSGFramework/Hediffs/HediffComp_HediffWhileMoving.cs
@@ -15,7 +15,8 @@ namespace EBSGFramework
                 {
                     if (Props.severityFadePerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.severityFadePerHour / 2500, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
                     else if (Props.notMovingSeverityFadePerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.notMovingSeverityFadePerHour / 2500, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
-                    else EBSGUtilities.FadingHediffs(Pawn, Props.notMovingSeverityPerHour / 2500 * -1, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
+                    else if (Props.notMovingSeverityPerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.notMovingSeverityPerHour / 2500 * -1, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
+                    else EBSGUtilities.RemoveHediffs(Pawn, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
                 }
                 else EBSGUtilities.RemoveHediffs(Pawn, Props.hediffWhileNotMoving, Props.hediffsWhileNotMoving);
             }
@@ -26,7 +27,8 @@ namespace EBSGFramework
                 {
                     if (Props.severityFadePerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.severityFadePerHour / 2500, Props.hediffWhileMoving, Props.hediffsWhileMoving);
                     else if (Props.movingSeverityFadePerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.movingSeverityFadePerHour / 2500, Props.hediffWhileMoving, Props.hediffsWhileMoving);
-                    else EBSGUtilities.FadingHediffs(Pawn, Props.movingSeverityPerHour / 2500 * -1, Props.hediffWhileMoving, Props.hediffsWhileMoving);
+                    else if (Props.movingSeverityPerHour > 0) EBSGUtilities.FadingHediffs(Pawn, Props.movingSeverityPerHour / 2500 * -1, Props.hediffWhileMoving, Props.hediffsWhileMoving);
+                    else EBSGUtilities.RemoveHediffs(Pawn, Props.hediffWhileMoving, Props.hediffsWhileMoving);
                 }
                 else EBSGUtilities.RemoveHediffs(Pawn, Props.hediffWhileMoving, Props.hediffsWhileMoving);
             }
