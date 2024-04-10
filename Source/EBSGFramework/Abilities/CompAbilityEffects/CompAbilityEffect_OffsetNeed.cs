86cc4cc5061d1c37d7d9ba88fb460b90922595c5
Robert <robhos2000@gmail.com>
2024-04-10 11:02:41 -0400

    Minor fix

--- Source/EBSGFramework/Abilities/CompAbilityEffects/CompAbilityEffect_OffsetNeed.cs
+++ Source/EBSGFramework/Abilities/CompAbilityEffects/CompAbilityEffect_OffsetNeed.cs
@@ -14,7 +14,7 @@ namespace EBSGFramework
             if (target.IsValid && target.HasThing && target.Thing is Pawn pawn && (!Props.psychic || pawn.GetStatValue(StatDefOf.PsychicSensitivity) > 0) && pawn.needs != null)
                 EBSGUtilities.HandleNeedOffsets(pawn, Props.needOffsets, Props.preventRepeats);
 
-            if (parent.pawn != null || (Props.psychic && parent.pawn.GetStatValue(StatDefOf.PsychicSensitivity) <= 0)) return;
+            if (parent.pawn == null || parent.pawn.needs == null || (Props.psychic && parent.pawn.GetStatValue(StatDefOf.PsychicSensitivity) <= 0)) return;
             EBSGUtilities.HandleNeedOffsets(parent.pawn, Props.casterNeedOffsets, Props.preventRepeats);
         }
     }
