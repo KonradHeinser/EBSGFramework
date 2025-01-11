3dca3107a04359f6d8b9b4f8ca75718890851580
KonradHeinser <robhos2000@gmail.com>
2025-01-11 13:32:55 -0800

    Added random shot verb

---
+++ Source/EBSGFramework/Things/Verb_RandomShot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class Verb_RandomShot : Verb_Shoot
+    {
+        public RandomShotExtension cachedExtension;
+
+        public RandomShotExtension Extension
+        {
+            get
+            {
+                if (cachedExtension == null)
+                {
+                    cachedExtension = HediffSource?.def.GetModExtension<RandomShotExtension>() ??
+                        EquipmentSource?.def.GetModExtension<RandomShotExtension>();
+                }
+
+                return cachedExtension;
+            }
+        }
+
+        public override ThingDef Projectile
+        {
+            get
+            {
+                return Extension?.projectiles?.RandomElementByWeight((arg) => arg.weight)?.projectile ?? base.Projectile;
+            }
+        }
+    }
+
+    public class RandomShotExtension : DefModExtension
+    {
+        public List<RandomProjectilePackage> projectiles = new List<RandomProjectilePackage>();
+    }
+
+    public class RandomProjectilePackage
+    {
+        public ThingDef projectile;
+        public int weight = 1;
+    }
+}
