f2c98e922b5fa7bf40fbdf5cc8f307ff0bc970a2
KonradHeinser <robhos2000@gmail.com>
2025-01-12 14:12:25 -0800

    Added invisible at stage

---
+++ Source/EBSGFramework/Hediffs/CompProperties/HediffCompProperties_InvisibleAtStage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class HediffCompProperties_InvisibleAtStage : HediffCompProperties
+    {
+        public List<int> invisibleStages;
+        public HediffCompProperties_InvisibleAtStage()
+        {
+            compClass = typeof(HediffComp_InvisibleAtStage);
+        }
+    }
+}
