b09d957ffc297e6b1cb7e58d2ff22166e13c166e
KonradHeinser <robhos2000@gmail.com>
2025-01-11 18:55:38 -0800

    Thought should now be properly inactive if below the first stage or 1 severity

--- Source/EBSGFramework/Thoughts/ThoughtWorker_HediffSeverity.cs
+++ Source/EBSGFramework/Thoughts/ThoughtWorker_HediffSeverity.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RimWorld;
+﻿using RimWorld;
 using Verse;
 
 namespace EBSGFramework
@@ -16,8 +11,12 @@ namespace EBSGFramework
 
             if (thoughtExtension?.hediff != null && thoughtExtension.curve != null)
             {
-                if (p.HasHediff(thoughtExtension.hediff))
-                    return ThoughtState.ActiveDefault;
+                if (p.HasHediff(thoughtExtension.hediff, out var hediff))
+                {
+                    if (hediff.def.stages.Count > 1)
+                        return hediff.Severity > hediff.def.stages[1].minSeverity;
+                    return hediff.Severity > 1f;
+                }
             }
 
             return ThoughtState.Inactive;
@@ -33,6 +32,18 @@ namespace EBSGFramework
                 return label.Formatted(p.Named("PAWN"), hediff.Label.Named("HEDIFF"));
             }
             return base.PostProcessLabel(p, label);
+        }
+
+        public override string PostProcessDescription(Pawn p, string description)
+        {
+            EBSGThoughtExtension thoughtExtension = def.GetModExtension<EBSGThoughtExtension>();
+            if (p.health.hediffSet.TryGetHediff(thoughtExtension.hediff, out var hediff))
+            {
+                if (hediff is Hediff_Dependency dependency)
+                    return description.Formatted(p.Named("PAWN"), dependency.Label.Named("HEDIFF"), dependency.GetLabel().Named("DEPENDENCY"));
+                return description.Formatted(p.Named("PAWN"), hediff.Label.Named("HEDIFF"));
+            }
+            return base.PostProcessDescription(p, description);
         }
     }
 }
