aa5c5f4d454f95be6ef99d98f7c25cb123b541b3
KonradHeinser <robhos2000@gmail.com>
2025-01-12 19:40:08 -0800

    Made stat parts to prep for mechanitor stats

---
+++ Source/EBSGFramework/Stats/StatPart_OverseerStatFactor.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EBSGFramework.Stats
+{
+    public class StatPart_OverseerStatFactor : StatPart
+    {
+        private StatDef stat;
+
+        [MustTranslate]
+        public string label;
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (GetFactor(req, out var factor))
+                return $"{label} : x{factor.ToStringByStyle(stat.toStringStyle, stat.toStringNumberSense)}";
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (GetFactor(req, out var factor))
+                val *= factor;
+        }
+
+        private bool GetFactor(StatRequest req, out float factor)
+        {
+            factor = 0f;
+
+            if (ModsConfig.BiotechActive && req.HasThing && req.Thing is Pawn pawn)
+            {
+                Pawn overseer = pawn.GetOverseer();
+                if (overseer != null)
+                {
+                    factor = overseer.GetStatValue(stat);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Source/EBSGFramework/Stats/StatPart_OverseerStatOffset.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class StatPart_OverseerStatOffset : StatPart
+    {
+        private StatDef stat;
+
+        [MustTranslate]
+        public string label;
+
+        public override string ExplanationPart(StatRequest req)
+        {
+            if (GetOffset(req, out var offset))
+                return $"{label} : +{offset.ToStringByStyle(stat.toStringStyle, stat.toStringNumberSense)}";
+            return null;
+        }
+
+        public override void TransformValue(StatRequest req, ref float val)
+        {
+            if (GetOffset(req, out var offset))
+                val += offset;
+        }
+
+        private bool GetOffset(StatRequest req, out float offset)
+        {
+            offset = 0f;
+
+            if (ModsConfig.BiotechActive && req.HasThing && req.Thing is Pawn pawn)
+            {
+                Pawn overseer = pawn.GetOverseer();
+                if (overseer != null)
+                {
+                    offset = overseer.GetStatValue(stat);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
