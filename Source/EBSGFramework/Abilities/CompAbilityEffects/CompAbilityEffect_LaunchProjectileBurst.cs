0bd0c9af3ea1cb9cbd05624cd84ebbb305fa864b
KonradHeinser <robhos2000@gmail.com>
2025-02-21 08:56:43 -0800

    Yoinked and upgraded projectile burst ability comp from Athena

---
+++ Source/EBSGFramework/Abilities/CompAbilityEffects/CompAbilityEffect_LaunchProjectileBurst.cs
@@ -0,0 +1,75 @@
+﻿using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace EBSGFramework
+{
+    public class CompAbilityEffect_LaunchProjectileBurst : CompAbilityEffect
+    {
+        public new CompProperties_AbilityLaunchProjectileBurst Props => (CompProperties_AbilityLaunchProjectileBurst)props;
+
+        public int ticksToShot = -1;
+
+        public int shotsLeft = 0;
+
+        public LocalTargetInfo curTarget;
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            shotsLeft = Props.burstCount;
+            curTarget = target;
+            if (Props.immediateLaunch)
+                LaunchProjectile(target);
+            ticksToShot = Props.ticksBetweenShots;
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref ticksToShot, "ticksToShot");
+            Scribe_Values.Look(ref shotsLeft, "shotsLeft");
+            Scribe_TargetInfo.Look(ref curTarget, false, "curTarget");
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (shotsLeft == 0)
+                return;
+
+            if (curTarget == null || !curTarget.IsValid || (curTarget.HasThing && (!curTarget.Thing.Spawned || curTarget.Thing.Destroyed)))
+            {
+                shotsLeft = 0;
+                curTarget = null;
+                return;
+            }
+
+            if (ticksToShot > 0)
+            {
+                ticksToShot--;
+                return;
+            }
+
+            ticksToShot = Props.ticksBetweenShots;
+            LaunchProjectile(curTarget);
+        }
+
+        public virtual void LaunchProjectile(LocalTargetInfo target)
+        {
+            shotsLeft--;
+            Projectile proj = GenSpawn.Spawn(Props.projectileDef, parent.pawn.Position, parent.pawn.Map) as Projectile;
+            proj.Launch(parent.pawn, parent.pawn.DrawPos, target, target, ProjectileHitFlags.IntendedTarget);
+
+            Props?.firingSound?.PlayOneShot(new TargetInfo(parent.pawn.Position, parent.pawn.Map));
+
+            if (shotsLeft == 0)
+                curTarget = null;
+        }
+
+        public override bool AICanTargetNow(LocalTargetInfo target)
+        {
+            return target.Thing != null;
+        }
+    }
+}
---
+++ Source/EBSGFramework/Abilities/CompProperties/CompProperties_AbilityLaunchProjectileBurst.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class CompProperties_AbilityLaunchProjectileBurst : CompProperties_AbilityEffect
+    {
+        public ThingDef projectileDef;
+
+        public int burstCount = 1;
+
+        public int ticksBetweenShots = 5;
+
+        public SoundDef firingSound;
+
+        public bool immediateLaunch = true;
+
+        public CompProperties_AbilityLaunchProjectileBurst()
+        {
+            compClass = typeof(CompAbilityEffect_LaunchProjectileBurst);
+        }
+
+        public override IEnumerable<string> ConfigErrors(AbilityDef parentDef)
+        {
+            if (burstCount < 1)
+                yield return "burstCount must be 1 or higher";
+            if (ticksBetweenShots < 1)
+                yield return "ticksBetweenShots must be 1 or higher";
+        }
+    }
+}
