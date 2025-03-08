41866a3f14e97370ac1ef589eecc232492977293
KonradHeinser <robhos2000@gmail.com>
2025-03-08 14:49:35 -0800

    Added autocast toggle ability comp and gizmo

---
+++ Source/EBSGFramework/EBSGAI/CompProperties_AbilityAutocastToggle.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+
+namespace EBSGFramework
+{
+    public class CompProperties_AbilityAutocastToggle : CompProperties_AbilityEffect
+    {
+        public string active = "EBSG/AutoOn";
+
+        public string inactive = "EBSG/AutoOff";
+
+        public CompProperties_AbilityAutocastToggle() 
+        { 
+            compClass = typeof(CompAbilityEffect_AutocastToggle);
+        }
+    }
+}
