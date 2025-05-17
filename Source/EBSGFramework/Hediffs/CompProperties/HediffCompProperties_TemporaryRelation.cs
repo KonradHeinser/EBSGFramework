3f0806c85f375cf23b239d713b042fa0a83def5b
KonradHeinser <robhos2000@gmail.com>
2025-05-17 15:55:13 -0700

    Added temporary relation hediff comp

---
+++ Source/EBSGFramework/Hediffs/CompProperties/HediffCompProperties_TemporaryRelation.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class HediffCompProperties_TemporaryRelation : HediffCompProperties
+    {
+        public PawnRelationDef ownRelation;
+
+        public PawnRelationDef otherRelation;
+
+        public HediffCompProperties_TemporaryRelation()
+        {
+            compClass = typeof(HediffComp_TemporaryRelation);
+        }
+    }
+}
---
+++ Source/EBSGFramework/Hediffs/Comps/HediffComp_TemporaryRelation.cs
@@ -0,0 +1,36 @@
+﻿using Verse;
+
+namespace EBSGFramework
+{
+    public class HediffComp_TemporaryRelation : HediffComp
+    {
+        public HediffCompProperties_TemporaryRelation Props => (HediffCompProperties_TemporaryRelation)props;
+
+        public override void CompPostPostAdd(DamageInfo? dinfo)
+        {
+            base.CompPostPostAdd(dinfo);
+            if (parent is HediffWithTarget p && p.target is Pawn other)
+            {
+                if (Props.ownRelation != null)
+                    Pawn.relations?.AddDirectRelation(Props.ownRelation, other);
+                if (Props.otherRelation != null)
+                    other.relations?.AddDirectRelation(Props.otherRelation, Pawn);
+            }
+            else
+                Log.Error($"{parent.def} must be a HediffWithTarget or child of that class, and it must target a pawn");
+        }
+
+        public override void CompPostPostRemoved()
+        {
+            base.CompPostPostRemoved();
+
+            if (parent is HediffWithTarget p && p.target is Pawn other)
+            {
+                if (Props.ownRelation != null)
+                    Pawn.relations?.TryRemoveDirectRelation(Props.ownRelation, other);
+                if (Props.otherRelation != null)
+                    other.relations?.TryRemoveDirectRelation(Props.otherRelation, Pawn);
+            }
+        }
+    }
+}
