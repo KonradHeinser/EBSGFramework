09789b499b193fb8b091fbfa249efac8e70994e0
KonradHeinser <robhos2000@gmail.com>
2025-07-22 15:55:16 -0700

    Setting min height

--- Source/EBSGFramework/Settings/EBSG_Settings.cs
+++ Source/EBSGFramework/Settings/EBSG_Settings.cs
@@ -448,7 +448,7 @@ namespace EBSGFramework
             contentRect.y = 0;
 
             if (tabInt == 3 && currentCategory != null)
-                contentRect.height = 30f + (flexibleSettings[currentCategory].Count + 1) * 30f;
+                contentRect.height = Mathf.Max(30f + (flexibleSettings[currentCategory].Count + 1) * 30f, contentRect.height);
 
             Widgets.BeginScrollView(frameRect, ref scrollPosition, contentRect);
             optionsMenu.Begin(contentRect.AtZero());
