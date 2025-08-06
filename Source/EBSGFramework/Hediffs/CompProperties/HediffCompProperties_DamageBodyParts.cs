4d2684842dce3d1a540f9d3f3f131e3ddb3e8f69
KonradHeinser <robhos2000@gmail.com>
2025-08-05 17:57:43 -0700

    Added count stuff to damage body part

--- Source/EBSGFramework/Hediffs/CompProperties/HediffCompProperties_DamageBodyParts.cs
+++ Source/EBSGFramework/Hediffs/CompProperties/HediffCompProperties_DamageBodyParts.cs
@@ -6,7 +6,12 @@ namespace EBSGFramework
     public class HediffCompProperties_DamageBodyParts : HediffCompProperties
     {
         public List<BodyPartDef> bodyPartsToRemove;
+
+        public int removeCount = 0;
+
         public List<PartToDamage> bodyPartsToDamage;
+
+        public int damageCount = 0;
 
         public HediffCompProperties_DamageBodyParts()
         {
--- Source/EBSGFramework/Hediffs/Comps/HediffComp_DamageBodyParts.cs
+++ Source/EBSGFramework/Hediffs/Comps/HediffComp_DamageBodyParts.cs
@@ -11,11 +11,14 @@ namespace EBSGFramework
         public override void CompPostTick(ref float severityAdjustment)
         {
             base.CompPostTick(ref severityAdjustment);
-            if (Pawn.IsHashIntervalTick(60)) // Wait a few ticks before removing just to be safe
+            if (Pawn.IsHashIntervalTick(62)) // Wait a few ticks before removing just to be safe
             {
                 if (!Props.bodyPartsToRemove.NullOrEmpty())
                 {
-                    foreach (BodyPartDef bodyPartDef in Props.bodyPartsToRemove)
+                    var parts = new List<BodyPartDef>(Props.bodyPartsToRemove);
+                    parts.Shuffle(); // Picking random parts requires shuffling
+                    int count = Props.removeCount;
+                    foreach (BodyPartDef bodyPartDef in parts)
                     {
                         BodyPartRecord bodyPart = null;
                         foreach (BodyPartRecord notMissingPart in Pawn.health.hediffSet.GetNotMissingParts())
@@ -26,18 +29,26 @@ namespace EBSGFramework
                                 break;
                             }
                         }
-                        if (bodyPart == null) continue; // If no part is found, just "continue" down the list
+                        if (bodyPart == null) 
+                            continue; // If no part is found, time to leave
                         Pawn.TakeDamage(new DamageInfo(EBSGDefOf.EBSG_GeneticDamage, 99999f, 999f, -1f, null, bodyPart, spawnFilth: false, preventCascade: true));
 
                         var missing = Pawn.health.hediffSet.GetMissingPartFor(bodyPart);
                         if (missing?.TendableNow() == true)
                             missing.Tended(1, 1);
+
+                        count--;
+                        if (count == 0) // Will never happen when the remove count starts at 0
+                            break;
                     }
                 }
                 if (!Props.bodyPartsToDamage.NullOrEmpty())
                 {
+                    var parts = new List<PartToDamage>(Props.bodyPartsToDamage);
+                    parts.Shuffle(); // Picking random parts requires shuffling
+                    int count = Props.damageCount;
                     List<BodyPartRecord> alreadyTargettedParts = new List<BodyPartRecord>();
-                    foreach (PartToDamage partToDamage in Props.bodyPartsToDamage)
+                    foreach (PartToDamage partToDamage in parts)
                     {
                         BodyPartRecord bodyPart = null;
                         foreach (BodyPartRecord notMissingPart in Pawn.health.hediffSet.GetNotMissingParts())
@@ -49,13 +60,18 @@ namespace EBSGFramework
                                 break;
                             }
                         }
-                        if (bodyPart == null) continue; // If no part is found, just "continue" down the list
+                        if (bodyPart == null) 
+                            continue; // If no part is found, time to leave
                         if (partToDamage.damagePercentage > 0) Pawn.TakeDamage(new DamageInfo(EBSGDefOf.EBSG_GeneticDamage, bodyPart.def.hitPoints * partToDamage.damagePercentage * Pawn.HealthScale, 999f, -1f, null, bodyPart));
                         else Pawn.TakeDamage(new DamageInfo(EBSGDefOf.EBSG_GeneticDamage, partToDamage.damageAmount, 999f, -1f, null, bodyPart));
 
                         var missing = Pawn.health.hediffSet.GetMissingPartFor(bodyPart);
                         if (missing?.TendableNow() == true)
                             missing.Tended(1, 1);
+
+                        count--;
+                        if (count == 0) // Will never happen when the remove count starts at 0
+                            break; 
                     }
                 }
                 Pawn.health.RemoveHediff(parent);
