9302a2535d0da852924577cec0569bb81164dc6e
KonradHeinser <robhos2000@gmail.com>
2025-09-26 16:10:22 -0700

    Hostile Give Hediff

---
+++ Source/EBSGFramework/Abilities/CompAbilityEffects/CompAbilityEffect_HostileGiveHediff.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace EBSGFramework
+{
+    public class CompAbilityEffect_HostileGiveHediff : CompAbilityEffect_GiveHediff
+    {
+        public override bool AICanTargetNow(LocalTargetInfo target)
+        {
+            if (target.Pawn?.TargetCurrentlyAimingAt != parent.pawn)
+                return false;
+            return base.AICanTargetNow(target);
+        }
+    }
+}
