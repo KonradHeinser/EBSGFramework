e3131434fbc75544493f7a2e6bd26268260c4c76
KonradHeinser <robhos2000@gmail.com>
2025-10-06 08:26:09 -0700

    ThinkNode_ConditionalAbilityCastLastTickOrNever

---
+++ Source/EBSGFramework/EBSGAI/ConditionalNodes/ThinkNode_ConditionalAbilityCastLastTickOrNever.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace EBSGFramework
+{
+    public class ThinkNode_ConditionalAbilityCastLastTickOrNever : ThinkNode_Conditional
+    {
+        public int maxTicksAgo = int.MaxValue;
+
+        public int minTicksAgo;
+
+        public AbilityDef ability;
+
+        protected override bool Satisfied(Pawn pawn)
+        {
+            Ability ability = pawn.abilities?.GetAbility(this.ability);
+            if (ability == null)
+                return false;
+
+            if (ability.lastCastTick < 0)
+                return true;
+
+            int num = Find.TickManager.TicksGame - ability.lastCastTick;
+            if (num > maxTicksAgo)
+                return false;
+
+            if (num < minTicksAgo)
+                return false;
+
+            return true;
+        }
+
+        public override ThinkNode DeepCopy(bool resolve = true)
+        {
+            ThinkNode_ConditionalAbilityCastLastTickOrNever obj = (ThinkNode_ConditionalAbilityCastLastTickOrNever)base.DeepCopy(resolve);
+            obj.ability = ability;
+            obj.maxTicksAgo = maxTicksAgo;
+            obj.minTicksAgo = minTicksAgo;
+            return obj;
+        }
+    }
+}
